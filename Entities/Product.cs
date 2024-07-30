3d4362b4ad2c7368476132364723fd222582faf2
matheus.domeneghetti <matheus.domeneghetti@RPOLOG0046>
2024-07-30 13:57:05 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExercicioPolimorfismo.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return Name + " $ " + Price;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using ExercicioPolimorfismo.Entities;
+using System;
+using System.Globalization;
+
+namespace ExercicioPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Product #" + i + " data:");
+                Console.Write("Commom, used or imported (c/u/i)");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (type == 'c')
+                {
+                    products.Add(new Product(name, price));
+                }
+                else if (type == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY) :");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("PRICE TAGS:");
+                foreach (Product product in products)
+                {
+                    Console.WriteLine(product.PriceTag());
+                }
+            }
+
+        }
+    }
+}
